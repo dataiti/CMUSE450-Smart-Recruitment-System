8141df32639fa06d95b8eda53be38365af0ac943
Phamtheduyet123 <duyetpham322@gmail.com>
2024-02-28 20:04:48 +0700

    push code 28/02/2024 lan 2

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Basic from "./components/Basic"
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <h1>Shreyas</h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
